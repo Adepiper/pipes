ed3263384a66251812b0aeea16b43ac05c79598e
Adepiper <52428747+Adepiper@users.noreply.github.com>
2019-11-18 00:43:45 +0100

    authguard working

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -12,11 +12,13 @@ export class AuthGuard implements CanActivate {
   }
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.auth.loginUser != null) {
-    return true;
+    state: RouterStateSnapshot): boolean {
+      if (localStorage.getItem('user') != null){
+        return true;
       } else {
-        this.router.navigate[('/Login')];
+        this.router.navigate(['/Login'])
+        return false;
       }
-  }
+    }
+
 }
