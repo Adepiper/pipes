1f59d5d80f560d18bfd3106423594fe90900daf0
Adepiper <52428747+Adepiper@users.noreply.github.com>
2019-11-18 00:48:21 +0100

    error

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthserviceService } from '../service/authservice.service';
+import { TOASTR_TOKEN, Toastr } from '../service/toastr.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private auth: AuthserviceService){
+  constructor(private router: Router, private auth: AuthserviceService, @Inject(TOASTR_TOKEN) private toastr: Toastr){
   }
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -16,7 +17,8 @@ export class AuthGuard implements CanActivate {
       if (localStorage.getItem('user') != null){
         return true;
       } else {
-        this.router.navigate(['/Login'])
+        this.router.navigate(['/Login']);
+        this.toastr.error('Please Log in');
         return false;
       }
     }
